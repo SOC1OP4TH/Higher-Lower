a9443a2986986bd8a9affcc6e9b0ad59268f5aa6
EminAbd <eabdulla@ualberta.ca>
2023-04-16 17:36:19 +0400

    Update Commit

---
+++ assignment/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+   public class Program
+    {
+        public static int GetRangedInput(int min, int max)
+        {
+            Random random = new Random();
+            if (min < 1 || max > 1000)
+                throw new ArgumentOutOfRangeException("min must be greater than 1", " max must be less than 1000");
+            return random.Next(min, max);
+         
+
+        }
+        public static bool ValidateRangedInput(int min, int max, int value)
+        {
+            if (value < min && value > max) { return false; }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+          
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int myrandomnuber = GetRangedInput(num1, num2);
+            if(!ValidateRangedInput(num1, num2, myrandomnuber))
+                return; 
+            bool isdone = false;
+          
+
+            while (!isdone)
+            {
+
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if(guess == myrandomnuber)
+                {
+                    isdone = true;
+                    Console.WriteLine("U have found the answer");
+                }
+                    
+            }
+
+
+        }
+    }
+    
+
+
+}
+
+
+
